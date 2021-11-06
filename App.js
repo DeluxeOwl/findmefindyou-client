000692f51babec0e2ea7ca88107c8af3bdf419c5
DeluxeOwl <nospamplease0@protonmail.com>
2021-11-06 19:34:09 +0200

    Added global state manager

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import * as eva from "@eva-design/eva";
+import {
+  ApplicationProvider,
+  Layout,
+  Text,
+  List,
+  ListItem,
+} from "@ui-kitten/components";
+import { StyleSheet } from "react-native";
+
+const data = new Array(8).fill({
+  title: "Item",
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <HomeScreen />
+      <ListSimpleUsageShowcase />
       <StatusBar style="auto" />
-    </View>
+    </ApplicationProvider>
   );
 }
 
+const HomeScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text category="h1">HOME</Text>
+  </Layout>
+);
+
+export const ListSimpleUsageShowcase = () => {
+  const renderItem = ({ item, index }) => (
+    <ListItem title={`${item.title} ${index + 1}`} />
+  );
+
+  return <List style={styles.container} data={data} renderItem={renderItem} />;
+};
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    maxHeight: 180,
   },
 });
