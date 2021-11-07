c2e4eaef528a740e69a57a2d2849e7774cf94bfb
DeluxeOwl <nospamplease0@protonmail.com>
2021-11-07 16:02:02 +0200

    Removed the go-to select

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,30 +1,13 @@
-import { IndexPath, Layout, Select, SelectItem } from "@ui-kitten/components";
+import { Layout } from "@ui-kitten/components";
 import React from "react";
 import AddUserID from "./AddUserID";
 import SendDataToggle from "./SendDataToggle";
 import UserList from "./UserList";
 
 export default function HomeScreen({ navigation }) {
-  const [selectedIndex, setSelectedIndex] = React.useState(new IndexPath(0));
-
-  React.useEffect(() => {
-    if (selectedIndex.row === 1) {
-      navigation.navigate("Map");
-    }
-  }, [selectedIndex]);
-
   return (
     <React.Fragment>
       <Layout style={{ flex: 1 }} level="1">
-        <Select
-          selectedIndex={selectedIndex}
-          onSelect={(index) => setSelectedIndex(index)}
-          value="Go to ..."
-        >
-          <SelectItem title="Doesn't go anywhere" />
-          <SelectItem title="Go to Map" />
-        </Select>
-
         <UserList />
         <AddUserID />
 
