e5bd1e5709a7f647beead36071b2cf5cbcb5950e
DeluxeOwl <nospamplease0@protonmail.com>
2021-11-08 17:46:06 +0200

    Added upload image button

--- components/BottomProfile.js
+++ components/BottomProfile.js
@@ -1,4 +1,4 @@
-import { Avatar, Button, Icon, Text } from "@ui-kitten/components";
+import { Avatar, Button, Icon, Text, Card, Modal } from "@ui-kitten/components";
 import React from "react";
 import { StyleSheet, View, Pressable } from "react-native";
 import * as RootNavigation from "../util/RootNavigation";
@@ -8,40 +8,65 @@ export default function BottomProfile({
   status,
   notifNumber,
 }) {
+  // State of the modal for image upload
+  const [visible, setVisible] = React.useState(false);
+  // We're using a reference to the react navigator to avoid
+  // some hook errors
   const handleNav = () => {
     RootNavigation.navigate("Notifications");
   };
   return (
-    <View style={styles.activityContainer}>
-      <Pressable onPress={() => console.warn("Pressed own avatar")}>
-        <Avatar
-          source={{
-            uri: avatarUri,
-          }}
-        />
-      </Pressable>
-      <View style={styles.authoringInfoContainer}>
-        <Text>{name}</Text>
-        <Text appearance="hint" category="p2">
-          {status}
-        </Text>
-      </View>
-      <Button
-        style={styles.iconButton}
-        appearance="ghost"
-        status="basic"
-        accessoryLeft={
-          <Icon
-            fill={(notifNumber !== 0 && "red") || "gray"}
-            stroke={(notifNumber !== 0 && "red") || "gray"}
-            name="bell-outline"
-          />
-        }
-        onPress={handleNav}
+    <React.Fragment>
+      <Modal
+        visible={visible}
+        backdropStyle={styles.backdrop}
+        style={{ width: "80%" }}
+        onBackdropPress={() => {
+          setVisible(false);
+        }}
       >
-        {notifNumber === 0 ? "" : `${notifNumber} new`}
-      </Button>
-    </View>
+        <Card disabled={true}>
+          <Button
+            style={styles.button}
+            appearance="outline"
+            status="primary"
+            accessoryLeft={<Icon name="cloud-upload-outline" />}
+          >
+            Upload image
+          </Button>
+        </Card>
+      </Modal>
+      <View style={styles.activityContainer}>
+        <Pressable onPress={() => setVisible(true)}>
+          <Avatar
+            source={{
+              uri: avatarUri,
+            }}
+          />
+        </Pressable>
+        <View style={styles.authoringInfoContainer}>
+          <Text>{name}</Text>
+          <Text appearance="hint" category="p2">
+            {status}
+          </Text>
+        </View>
+        <Button
+          style={styles.iconButton}
+          appearance="ghost"
+          status="basic"
+          accessoryLeft={
+            <Icon
+              fill={(notifNumber !== 0 && "red") || "gray"}
+              stroke={(notifNumber !== 0 && "red") || "gray"}
+              name="bell-outline"
+            />
+          }
+          onPress={handleNav}
+        >
+          {notifNumber === 0 ? "" : `${notifNumber} new`}
+        </Button>
+      </View>
+    </React.Fragment>
   );
 }
 const styles = StyleSheet.create({
@@ -56,5 +81,11 @@ const styles = StyleSheet.create({
   },
   iconButton: {
     paddingHorizontal: 0,
+  },
+  backdrop: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  button: {
+    margin: 5,
   },
 });
