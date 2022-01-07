cc5c10d7620dbe792ffb15ddf63971dd222dd0a8
DeluxeOwl <nospamplease0@protonmail.com>
2022-01-07 18:51:05 +0200

    adjusted some values

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 // Setting env variables in expo is finnicky, might as well use this
 
-const BACKEND_URL = "http://192.168.0.105:8000";
-const UPDATE_REMOTE_DB_INTERVAL_SECONDS = 90;
+const BACKEND_URL = "http://129.159.200.199:8066";
+const UPDATE_REMOTE_DB_INTERVAL_SECONDS = 60;
 const FOREGROUND_LOCATION_SECONDS = 30;
 
 export default {
